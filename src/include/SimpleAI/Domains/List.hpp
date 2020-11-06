88bf5f13c95024ea14d59c03f7efc68b9814f151
Денис Морозов <morden28@yandex.ru>
2020-11-06 15:42:42 +0300

    Added comments.

--- src/include/SimpleAI/Domains/List.hpp
+++ src/include/SimpleAI/Domains/List.hpp
@@ -5,14 +5,41 @@
 
 namespace SimpleAI {
 
+//! \brief Список вещественных чисел
 class List : public std::vector<double>
 {
 public:
+  //! \brief Конструктор по-умолчанию создает пустой список
   List();
+
+  /*!
+   * \brief Конструктор с параметром размера списка
+   * \details Создает список заданного размера инициализируя его значениями по-умолчанию
+   * \param size Размер списка
+   */
   List(const size_t& size);
+
+  /*!
+   * \brief Конструктор со списком инициализации
+   * \details Создает список вещественных чисел по списку инициализации
+   * \param list Список инициализации
+   */
   List(const std::initializer_list<double>& list);
 
+  /*!
+   * \brief Оператор ввода
+   * \param stream Поток из которого читать значения
+   * \param list Список в который записывать значения
+   * \return std::istream& Поток после ввода
+   */
   friend std::istream& operator>>(std::istream& stream, List& list);
+
+  /*!
+   * \brief Оператор вывода
+   * \param stream Поток в который записывать значения
+   * \param list Список из которого читать значения
+   * \return std::ostream& Поток после вывода
+   */
   friend std::ostream& operator<<(std::ostream& stream, const List& list);
 
 };
--- src/include/SimpleAI/Domains/Matrix.hpp
+++ src/include/SimpleAI/Domains/Matrix.hpp
@@ -5,13 +5,34 @@
 
 namespace SimpleAI {
 
+//! \brief Класс матрицы
 class Matrix : public std::vector<List>
 {
 public:
+  //! \brief Конструктор по-умолчанию создает пустую матрицу
   Matrix();
+
+  /*!
+   * \brief Конструктор со списком инициализации
+   * \details Создает матрицу по списку инициализации
+   * \param list Список инициализации
+   */
   Matrix(const std::initializer_list<List>& list);
 
+  /*!
+   * \brief Оператор ввода
+   * \param stream Поток из которого читать значения
+   * \param matrix Матрица в которую записывать значения
+   * \return std::istream& Поток после ввода
+   */
   friend std::istream& operator>>(std::istream& stream, Matrix& matrix);
+
+  /*!
+   * \brief Оператор вывода
+   * \param stream Поток в который записывать значения
+   * \param matrix Матрица из которой читать значения
+   * \return std::ostream& Поток после вывода
+   */
   friend std::ostream& operator<<(std::ostream& stream, const Matrix& matrix);
 
 };
